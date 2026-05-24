439c1a08b6f3934636558805294f3ec66e69e2fb
felipematheus1337 <lipehbr@gmail.com>
2026-05-24 20:49:29 -0300

    refactor the trivia endpoint with IA Prompt Engineering.

--- src/main/java/poc_springai/chat_ai/ChatAiApplication.java
+++ src/main/java/poc_springai/chat_ai/ChatAiApplication.java
@@ -7,22 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatAiApplication implements CommandLineRunner {
-
-	@Autowired
-	ChatClient.Builder builder;
+public class ChatAiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		var chatClient = builder.build();
-		var prompt = chatClient.prompt("Hey, tell me a funny Dad joke!");
-		var response = prompt.call().content();
-
-		System.out.println(response);
 	}
 }
--- src/main/java/poc_springai/chat_ai/SimpsonsExpert.java
+++ src/main/java/poc_springai/chat_ai/SimpsonsExpert.java
@@ -1,21 +1,36 @@
 package poc_springai.chat_ai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpsonsExpert {
 
-    ChatClient client;
+    ChatClient chatClient;
 
     public SimpsonsExpert(ChatClient.Builder builder) {
-        client = builder.build();
+        chatClient = builder.build();
     }
 
+    @GetMapping("/trivia")
     public String trivia(@RequestParam String prompt) {
 
-        return "Here is your prompt "  +prompt;
+        var systemMessage = """
+                You are a trivia expert specialized in only the TV show The Simpsons
+                
+                Rules:
+                - Only answer questions related to The simpsons.
+                - Only provide Simpsons related trivia.
+                - If the question is not about The Simpsons, respond with:
+                  "I can only answer questions about the Simpsons!"
+                - Do not answer non-Simpsons related questions;
+                """;
+        var fullPrompt = chatClient.prompt().system(systemMessage)
+                .user(prompt);
+
+        return fullPrompt.call().content();
     }
 
 
